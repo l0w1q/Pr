c981ba6c36b30bf0e84fdadd880f934acbcb407e
l0w1q <danbogdan2512@gmail.com>
2025-02-01 03:44:44 +0300

    Delete Dan-235-pr9 directory

--- Dan-235-pr9/DanPrimer/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ru.dan.danprimer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens ru.dan.danprimer to javafx.fxml;
-    exports ru.dan.danprimer;
-}
